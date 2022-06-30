40d34915add8893e4c6460d9fa4a1854b8520dd6
wood <amper.sil.tok@gmail.com>
2022-06-30 08:10:09 +0300

    added an abstract class for creating a tkinter frame

--- client/base.py
+++ client/base.py
@@ -1,7 +1,23 @@
+import abc
 from tkinter import Tk, ttk
+from abc import ABC
 
 
-class TkinterBaseFrame:
+class FramesConstruct(ABC):
+    """ABC class for constructing windows and frames in tkinter"""
+
+    @abc.abstractmethod
+    def run(self):
+        """Method to start run main loop tkinter"""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_frame_constructing(self):
+        """Method to collecting all construction frames"""
+        raise NotImplementedError
+
+
+class TkinterBaseFrame(FramesConstruct):
     """Base for all frames"""
 
     def __init__(self):
@@ -12,3 +28,7 @@ class TkinterBaseFrame:
     def run(self):
         """Start show app"""
         self.root.mainloop()
+
+    def get_frame_constructing(self):
+        """Constructing frames"""
+        pass
